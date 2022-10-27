edc10ea4e13df402db728e1e5bd11e790d7ed979
Anton Chernov <malikalhack@gmail.com>
2022-10-28 01:08:05 +0300

    Added solution files Added project files

---
+++ Common/inc/coordinate.h
@@ -0,0 +1,12 @@
+/**
+ * @file    coordinate.h
+ * @version 1.0.0
+ * @authors Anton Chernov
+ * @date    27/10/2022
+ * @date    28/10/2022
+ */
+
+#ifndef COORDINATE_H
+#define COORDINATE_H
+
+#endif

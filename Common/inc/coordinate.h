b24be08c7347df64979b103ff813cbdc73a36838
Anton Chernov <malikalhack@gmail.com>
2022-10-29 23:10:17 +0300

    Added implementation for all functions

--- Common/inc/coordinate.h
+++ Common/inc/coordinate.h
@@ -3,7 +3,7 @@
  * @version 1.0.0
  * @authors Anton Chernov
  * @date    27/10/2022
- * @date    28/10/2022
+ * @date    29/10/2022
  */
 
 #ifndef COORDINATE_H
@@ -11,25 +11,34 @@
 /****************************** Included files ********************************/
 #include <stdint.h>
 /******************************** Definition **********************************/
-#define X_LIMIT     (100)
-#define Y_LIMIT     (100)
-#define X_CENTER    (X_LIMIT/2)
-#define Y_CENTER    (Y_LIMIT/2)
+#define X_LIMIT     (200u)
+#define Y_LIMIT     (200u)
+
+#if X_LIMIT < 2 || Y_LIMIT < 2
+    #error Field limits are wrong
+#endif //Checking limits
+
+#define X_CENTER    (X_LIMIT/2u)
+#define Y_CENTER    (Y_LIMIT/2u)
+
+typedef uint16_t coordinate_t;
+typedef signed short offset_t;
+typedef unsigned short abs_offset_t;
 
 typedef struct {
-    uint16_t x; ///< x-coordinate of position
-    uint16_t y; ///< y-coordinate of position
+    coordinate_t x; ///< x-coordinate of position
+    coordinate_t y; ///< y-coordinate of position
 } Coordinate;
 /*************** Application Programming Interface prototypes *****************/
-void Coordinate_ctor(Coordinate * const, uint16_t, uint16_t);
-void MoveToCoordinate(Coordinate * const, uint16_t, uint16_t);
-void MoveFromCurrentPoint(Coordinate * const, int16_t, int16_t);
+void Coordinate_ctor(Coordinate * const, coordinate_t, coordinate_t);
+void MoveToCoordinate(Coordinate * const, coordinate_t, coordinate_t);
+void MoveFromCurrentPoint(Coordinate * const, offset_t, offset_t);
 void StandInTheLowerLeftCorner(Coordinate * const);
 void StandInTheUpperLeftCorner(Coordinate * const);
 void StandInTheLowerRightCorner(Coordinate * const);
 void StandInTheUpperRightCorner(Coordinate * const);
 void StandInTheCenter(Coordinate * const);
-uint16_t GetXCoordinate(Coordinate * const);
-uint16_t GetYCoordinate(Coordinate * const);
+coordinate_t GetXCoordinate(Coordinate * const);
+coordinate_t GetYCoordinate(Coordinate * const);
 /******************************************************************************/
 #endif
--- Common/src/coordinate.c
+++ Common/src/coordinate.c
@@ -3,52 +3,79 @@
  * @version 1.0.0
  * @authors Anton Chernov
  * @date    27/10/2022
- * @date    28/10/2022
+ * @date    29/10/2022
  */
 /****************************** Included files ********************************/
 #include "coordinate.h"
-/********************* Application Programming Interface **********************/
-void Coordinate_ctor(Coordinate * const self, uint16_t x, uint16_t y) {
-
+#include <assert.h>
+/****************************** Private  functions ****************************/
+static abs_offset_t abs(offset_t num) {
+    return (num < 0 ? num *= -1 : (abs_offset_t)num);
 }
 /*----------------------------------------------------------------------------*/
-void MoveToCoordinate(Coordinate * const self, uint16_t x, uint16_t y) {
-
+static void make_move(
+    coordinate_t * cc,
+    offset_t * offset,
+    coordinate_t const limit
+) {
+    if (*offset) {
+        abs_offset_t abs_offset = abs(*offset);
+        if (*offset < 0 && abs_offset >= *cc) { *cc = 0u; }
+        else if (*offset > 0 && (limit - *cc) <= abs_offset) { *cc = limit; }
+        else { *cc += *offset; }
+    }
+}
+/********************* Application Programming Interface **********************/
+void Coordinate_ctor(Coordinate * const self, coordinate_t x, coordinate_t y) {
+    assert(x <= X_LIMIT && y <= Y_LIMIT);
+    self->x = x;
+    self->y = y;
+}
+/*----------------------------------------------------------------------------*/
+void MoveToCoordinate(Coordinate * const self, coordinate_t x, coordinate_t y) {
+    self->x = x < X_LIMIT ? x : X_LIMIT;
+    self->y = y < Y_LIMIT ? y : Y_LIMIT;
 }
 /*----------------------------------------------------------------------------*/
 void MoveFromCurrentPoint(
     Coordinate * const self,
-    int16_t x_offset,
-    int16_t y_ofset
+    offset_t x_offset,
+    offset_t y_offset
 ) {
-
+    make_move(&self->x, &x_offset, X_LIMIT);
+    make_move(&self->y, &y_offset, Y_LIMIT);
 }
 /*----------------------------------------------------------------------------*/
 void StandInTheLowerLeftCorner(Coordinate * const self) {
-
+    self->x = 0u;
+    self->y = 0u;
 }
 /*----------------------------------------------------------------------------*/
 void StandInTheUpperLeftCorner(Coordinate * const self) {
-
+    self->x = 0u;
+    self->y = Y_LIMIT;
 }
 /*----------------------------------------------------------------------------*/
 void StandInTheLowerRightCorner(Coordinate * const self) {
-
+    self->x = X_LIMIT;
+    self->y = 0u;
 }
 /*----------------------------------------------------------------------------*/
 void StandInTheUpperRightCorner(Coordinate * const self) {
-
+    self->x = X_LIMIT;
+    self->y = Y_LIMIT;
 }
 /*----------------------------------------------------------------------------*/
 void StandInTheCenter(Coordinate * const self) {
-
+    self->x = X_CENTER;
+    self->y = Y_CENTER;
 }
 /*----------------------------------------------------------------------------*/
-uint16_t GetXCoordinate(Coordinate * const self) {
-    return 0;
+coordinate_t GetXCoordinate(Coordinate * const self) {
+    return self->x;;
 }
 /*----------------------------------------------------------------------------*/
-uint16_t GetYCoordinate(Coordinate * const self) {
-    return 0;
+coordinate_t GetYCoordinate(Coordinate * const self) {
+    return self->y;
 }
 /******************************************************************************/

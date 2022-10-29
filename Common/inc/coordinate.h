5a315dc4ed989afce68d7607174ea08a846a8226
Anton Chernov <malikalhack@gmail.com>
2022-10-29 23:33:46 +0300

    Added doxygen stuff

--- Common/inc/coordinate.h
+++ Common/inc/coordinate.h
@@ -30,15 +30,77 @@ typedef struct {
     coordinate_t y; ///< y-coordinate of position
 } Coordinate;
 /*************** Application Programming Interface prototypes *****************/
+
+/**
+ * @brief Constructor function
+ * @details Initializes the specified object with the passed coordinates.
+ * param[in] self - a pointer to the specified object
+ * param[in] x - the passed X-coordinate
+ * param[in] y - the passed Y-coordinate
+ * @note The coordinates are limited. An exception will be thrown if you try to
+ * go beyond the limit.
+ */
 void Coordinate_ctor(Coordinate * const, coordinate_t, coordinate_t);
+
+/**
+ * @brief Function for moving along the specified coordinates.
+ * param[in] self - a pointer to the specified object
+ * param[in] x - the passed new X-coordinate
+ * param[in] y - the passed new Y-coordinate
+ * @note The coordinates are limited. You cannot exceed the limit.
+ */
 void MoveToCoordinate(Coordinate * const, coordinate_t, coordinate_t);
+
+/**
+ * @details The function of moving from the current coordinates
+ * to the specified offset.
+ * param[in] self - a pointer to the specified object
+ * param[in] x_offset - the passed X-offset
+ * param[in] y_offset - the passed Y-offset
+ * @note The coordinates are limited. You cannot exceed the limit.
+ */
 void MoveFromCurrentPoint(Coordinate * const, offset_t, offset_t);
+
+/**
+ * @brief The function of moving to the lower left corner of the area.
+ * param[in] self - a pointer to the specified object
+ */
 void StandInTheLowerLeftCorner(Coordinate * const);
+
+/**
+ * @brief The function of moving to the upper left corner of the area.
+ * param[in] self - a pointer to the specified object
+ */
 void StandInTheUpperLeftCorner(Coordinate * const);
+
+/**
+ * @brief The function of moving to the lower right corner of the area.
+ * param[in] self - a pointer to the specified object
+ */
 void StandInTheLowerRightCorner(Coordinate * const);
+
+/**
+ * @brief The function of moving to the upper right corner of the area.
+ * param[in] self - a pointer to the specified object
+ */
 void StandInTheUpperRightCorner(Coordinate * const);
+
+/**
+ * @brief The function of moving to the centre of the area.
+ * param[in] self - a pointer to the specified object
+ */
 void StandInTheCenter(Coordinate * const);
+
+/**
+ * @brief The function to return the X-coordinate for the specified object.
+ * param[in] self - a pointer to the specified object
+ */
 coordinate_t GetXCoordinate(Coordinate * const);
+
+/**
+ * @brief The function to return the Y-coordinate for the specified object.
+ * param[in] self - a pointer to the specified object
+ */
 coordinate_t GetYCoordinate(Coordinate * const);
 /******************************************************************************/
 #endif
